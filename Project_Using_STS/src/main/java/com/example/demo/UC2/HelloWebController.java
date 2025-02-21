5314cbd39f4d47ae9e3712c9d5fe3c074db7d2cd
Abhishek Kumar <rajabhishek7654@gmail.com>
2025-02-21 14:58:12 +0530

    [Abhishek] add. Spring Learning Creating Hello World Spring App in Web Page

---
+++ Project_Using_STS/src/main/java/com/example/demo/UC2/HelloWebController.java
@@ -0,0 +1,18 @@
+package com.example.demo.UC2;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloWebController {
+    @GetMapping("/web")
+    public String helloWeb() {
+        return "hello";
+    }
+
+    @GetMapping("/web/message")
+    public String message(Model model) {
+        model.addAttribute("message", "This is a custom message");
+        return "message";
+    }
+}
