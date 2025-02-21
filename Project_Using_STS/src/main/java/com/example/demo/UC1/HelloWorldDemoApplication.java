97361863e19148f90a5ec1653b1ca49ffb9fcac7
Abhishek Kumar <rajabhishek7654@gmail.com>
2025-02-21 14:43:23 +0530

    [Abhishek] add. Spring Learning Creating HelloWorld Spring App

---
+++ Project_Using_STS/src/main/java/com/example/demo/UC1/HelloWorldDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo.UC1;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldDemoApplication {
+
+	public static void main(String[] args) {
+		org.springframework.boot.SpringApplication.run(HelloWorldDemoApplication.class, args);
+	}
+
+}
