bdb3d35e7ccf30c7b93475349ec998e22c2946c0
Abhishek Kumar <rajabhishek7654@gmail.com>
2025-02-21 15:02:34 +0530

    [Abhishek] add. Spring Learning Creating Hello World Spring App in Rest Call

---
+++ Project_Using_STS/src/main/java/com/example/demo/UC3/HelloWorldDemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo.UC3;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldDemoApplication {
+
+	public static void main(String[] args) {
+		org.springframework.boot.SpringApplication.run(HelloWorldDemoApplication.class, args);
+	}
+
+}
